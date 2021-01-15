8e7297d7c656cb24128a3c8fe5da283f8b32c3e6
mehbubrashid <mehbub.rabu@gmail.com>
2021-01-16 03:27:15 +0600

    deleted apple

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Apples from './Apples'
 
 function App() {
     return (
         <div>
             React with wordpress running awesome!
-            <Apples></Apples>
         </div>
     )
 }
